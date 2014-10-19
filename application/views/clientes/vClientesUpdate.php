d08b49ef5201ea10bbc92aafffe2068e0e908c63
LuisBriseno <pod_1180@hotmail.com>
2014-10-19 15:40:24 -0500

    Actualizacion en la estructura del proyecto

---
+++ application/views/clientes/vClientesUpdate.php
@@ -0,0 +1,17 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+
+<div id="container">
+	
+</div>
+
+</body>
+</html>
