9ed454723cc9910c173d6909df7be9d34286b740
Publialem <luis.briseno@alem.com.mx>
2014-11-06 12:18:47 -0600

    diseño login

--- application/views/main/vLogin.php
+++ application/views/main/vLogin.php
@@ -8,21 +8,39 @@ $form_login=array('id'=>'form_login');
 
 echo form_open('main/cLogin/login',$form_login);
 ?>
-	<table>
-	<!--inicio Datos del Cliente -->
-		<tr>
-			<td><?php echo form_label('Usuario: ','usr_nombre');?></td>
-			<td><?php echo form_input($usr_nombre);?></td>
-		</tr>
-		<tr>
-			<td><?php echo form_label('Contraseña: ','usr_passw');?></td>
-			<td><?php echo form_password($usr_passw);?></td>
-		</tr>
-	<!--fin Datos del Cliente -->
-	<tr>
-		<td><?php echo form_submit('login','Entrar','class="enviarButton"');?></td>
-	</tr>
-	</table>
+
+<div class="container">
+	<div class="row"><br><br><br><br></div>
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<center><img src="http://www.solarisdemexico.com/images/logo.png" />
+			<br><br>
+			<h2>Sistema Administrativo</h2></center>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+	
+	
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="jumbotron">
+				<!--inicio Datos del Cliente -->
+					<p><?php echo form_label('Usuario: ','usr_nombre');?></p>
+					<p><?php echo form_input($usr_nombre);?></p>
+					<p><?php echo form_label('Contraseña: ','usr_passw');?></p>
+					<p><?php echo form_password($usr_passw);?></p>
+				<!--fin Datos del Cliente -->
+				<center><div class="btn btn-default"><?php echo form_submit('login','Entrar','class="enviarButton"');?></div></center>
+				</div>
+			<div class="col-md-4"></div>
+		</div>
+	
+		
+			
+	
+</div>
 <?php
 echo form_close();
 echo getFooter() ;
