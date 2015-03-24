fba54eaad6c2e1eff328c7c9a3984cfc595f8e05
LuisBriseno <pod_1180@hotmail.com>
2015-03-23 19:16:44 -0600

    update

--- application/models/reportes/mremisionnote.php
+++ application/models/reportes/mremisionnote.php
@@ -13,13 +13,12 @@ class MRemisionNote extends CI_Model {
 		$query = $this->db->query('SELECT clientes.nombre_cliente,direcciones.calle,direcciones.numero_ext,direcciones.numero_int,telefonos.numero_telefono,
 									direcciones.municipio,estados.nombre_estado,sucursales.nombre_sucursal,usuarios.nombre_usuario,
 									remisiones.fecha,remisiones.iva,remisiones.total,productoremision.descuento,productoremision.cantidad,
-									productoremision.precio_actual,productos.nombre_producto,tipopagos.nombre_tipoPago,
+									productoremision.precio_actual,tipopagos.nombre_tipoPago,
 									(productoremision.cantidad * productoremision.precio_actual) as importe, 
 									SUM(productoremision.cantidad*productoremision.precio_actual) as subtotal,
 									ROUND(SUM(productoremision.cantidad*productoremision.precio_actual)*(remisiones.iva/100)+sum(productoremision.cantidad*productoremision.precio_actual),2) as totalf 
 									FROM remisiones
 									JOIN productoremision ON remisiones.id_remision = productoremision.id_remision
-									JOIN productos ON productoremision.id_producto = productos.id_producto
 									JOIN tipopagos ON remisiones.id_tipoPago = tipopagos.id_tipoPago
 									JOIN sucursales ON remisiones.id_sucursal = sucursales.id_sucursal
 									JOIN usuarios ON remisiones.creado_por = usuarios.id_usuario
@@ -31,6 +30,25 @@ class MRemisionNote extends CI_Model {
 									//AND usuarios.id_usuario = '.$idUsuario
 									);
 
+		if($query->num_rows()>0){
+			return $query;
+		}
+		else{
+			return false;
+		}
+	}
+
+	public function getProductoRemision($id_remision){
+		$query = $this->db->query('
+		SELECT productoremision.descuento,productoremision.cantidad,
+		productoremision.precio_actual,productos.nombre_producto,
+		(productoremision.cantidad * productoremision.precio_actual) as importe
+		FROM remisiones
+		JOIN productoremision ON remisiones.id_remision = productoremision.id_remision
+		JOIN productos ON productoremision.id_producto = productos.id_producto
+		WHERE remisiones.id_remision ="'.$id_remision.'"	
+		');
+		
 		if($query->num_rows()>0){
 			return $query;
 		}
